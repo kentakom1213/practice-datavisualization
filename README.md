c86c4708ea6ee365f77a86678cd582582acd14b7
kentakom1213 <kentakom1213@gmail.com>
2022-07-12 00:06:41 +0900

    [add] matplotlib資料の追加

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Pythonによるデータ可視化入門
 
 ## リソース
 - [Pythonによるデータ可視化入門](https://kentakom1213.github.io/practice-datavisualization/intro.html)
+- [12. Matplotlib入門 - ChainerTutorial](https://tutorials.chainer.org/ja/12_Introduction_to_Matplotlib.html)
---
+++ python-datavisualization/_build/jupyter_execute/matplotlib_advanced.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Matplotlib（応用編）
+# 
+# [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]()
+# 
+# ここでは、**オブジェクト指向**インターフェースを用いて、より発展的なグラフィックを作成します。
+# 
+# ```{note} オブジェクト指向とは？
+# ```
+
+# ## 参考
+# 
+# - [早く知っておきたかったmatplotlibの基礎知識、あるいは見た目の調整が捗るArtistの話 - Qiita](https://qiita.com/skotaro/items/08dc0b8c5704c94eafb9)
+# - [matplotlib入門 - りんごがでている](https://bicycle1885.hatenablog.com/entry/2014/02/14/023734)
